6c9dfca56e3924a1670d43306bbf423e88d32dbc
TheGreatAlgo <37487508+TheGreatAlgo@users.noreply.github.com>
2025-10-16 11:31:15 -0400

    Revert "fix: update default locale"
    This reverts commit c923f87879b5ebb6b7b41247d4bca5b173ab6943.

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -27,7 +27,7 @@ import {
   min,
   max,
 } from 'date-fns';
-import defaultLocale from 'date-fns/locale/en-US';
+import { enUS as defaultLocale } from 'date-fns/locale/en-US';
 import coreStyles from '../../styles';
 import { ariaLabelsShape } from '../../accessibility';
 
